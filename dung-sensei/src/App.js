d77268c3359380483358cbf48ec5306586ab4a5e
Ngo Van Thuc <ngothuc0704@gmail.com>
2024-04-10 06:12:25 +0700

    Window Resize React

--- dung-sensei/src/App.js
+++ dung-sensei/src/App.js
@@ -1,48 +1,21 @@
 import './App.css';
-import { Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
-
-const orders = [
-  {
-    id: "ORD001",
-    customerId: "C001",
-    date: "2021-03-21",
-  },
-  {
-    id: "ORD002",
-    customerId: "C002",
-    date: "2021-03-11",
-  },
-  {
-    id: "ORD003",
-    customerId: "C003",
-    date: "2021-06-14",
-  },
-];
+import { useState, useEffect } from 'react';
 
 export default function App() {
-  return (
-    <div style={{display: "flex", justifyContent: "center"}}>
-      <div style={{ width: "500px", borderStyle: "solid", borderWidth: "1px" }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell style={{ fontWeight: "bold" }}>ID</TableCell>
-              <TableCell style={{ fontWeight: "bold" }}>CustomerID</TableCell>
-              <TableCell style={{ fontWeight: "bold" }}>Date</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {orders.map(order => (
-              <TableRow>
-                <TableCell>{order.id}</TableCell>
-                <TableCell>{order.customerId}</TableCell>
-                <TableCell>{order.date}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-    </div>
-  );
-}
+  const [width, setWidth] = useState(window.innerWidth);
 
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
+  return <>Window Width: {width}</>
+
+
+}

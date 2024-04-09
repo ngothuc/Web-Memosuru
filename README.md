cc784c57c2cbfc3feee4165295b404b449810594
Ngo Van Thuc <ngothuc0704@gmail.com>
2024-04-10 02:30:39 +0700

    Structure

--- README.md
+++ README.md
@@ -4,6 +4,21 @@
 2. [Javascript cơ bản](https://www.youtube.com/playlist?list=PLncHg6Kn2JT5dfQqpVtfNYvv3EBVHHVKo)
 3. [React](https://www.youtube.com/playlist?list=PLncHg6Kn2JT4C0enPGQPK7ZIlEoZ1ZvRy)
 
+## Cách cài đặt
+- Mở cmd:
+> npm install -g create-react-app
+- Vào folder chưa project:
+> create-react-app my-app
+
+*Note: my-app là tên project, có thể thay đổi cho phù hợp*
+
+- Đợi cài đặt xong
+![](./TaiLieu/pictures/create_react_app.PNG)
+
+- Để chạy project:
+> cd my-app
+> npm start
+
 Cách các mục được sắp xếp trong repo này:
 - Lý thuyết sẽ đặt trong folder tài liệu. Từng mục như React, JavaScript, ... sẽ có thư mục riêng
 - Bài tập sẽ có thư mục riêng, không nằm trong mục tài liệu.
--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -10,16 +11,11 @@ class Square extends React.Component {
         };
     }
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.value !== this.props.value) {
-            this.setState({ value: this.props.value });
-        }
-    }
-
     render() {
         return (
-            <button className='square'
-            onClick={() => this.props.onClick()}>
+            <button 
+            className='square'
+            onClick={this.onClick()}>
                 {this.state.value}
             </button>
         )
@@ -27,30 +23,36 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+
     renderSquare(i) {
         return (
-            <Square 
-                key={i}
-                value = {this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-            />
+            <Square value = {i}></Square>
         )
+    }
+
+    handleClick(i) {
+        this.setState({
+            value: {i},
+        });
     }
 
     render() {
         return (
             <div>
-                <div className='board-row'>
+                <div className='board-row' 
+                onClick={(i) => {this.handleClick(i)}}>
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
                 </div>
-                <div className='board-row'>
+                <div className='board-row' 
+                onClick={(i) => {this.handleClick(i)}}>
                     {this.renderSquare(3)}
                     {this.renderSquare(4)}
                     {this.renderSquare(5)}
                 </div>
-                <div className='board-row'>
+                <div className='board-row' 
+                onClick={(i) => {this.handleClick(i)}}>
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
@@ -58,40 +60,7 @@ class Board extends React.Component {
             </div>
         )
     }
-};
+}
 
-class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            players: ['X', 'O'],
-            turn: 0,
-            squares: Array(9).fill(0),
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.players[this.state.turn % 2];
-        this.setState({
-            squares: squares,
-            value: this.state.players[this.state.turn % 2],
-            turn: this.state.turn + 1,
-        });
-    }
-
-    render() {
-        return (
-            <div className='status'>
-                Next Player: {this.state.players[this.state.turn % 2]}
-                <Board 
-                    squares={this.state.squares}
-                    onClick={(i) => this.handleClick(i)}
-                />
-            </div>
-        )
-    }
-};
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Game />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Board />)

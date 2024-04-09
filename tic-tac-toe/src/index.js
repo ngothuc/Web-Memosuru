9d9e0daecd14def09f6f37414a4c808c5e409254
Ngo Van Thuc <ngothuc0704@gmail.com>
2024-04-09 13:56:52 +0700

    tic-tac-toe by myself

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -10,10 +10,16 @@ class Square extends React.Component {
         };
     }
 
+    componentDidUpdate(prevProps) {
+        if (prevProps.value !== this.props.value) {
+            this.setState({ value: this.props.value });
+        }
+    }
+
     render() {
         return (
             <button className='square'
-            onClick={() => this.setState({value: this.state.value + 1})}>
+            onClick={() => this.props.onClick()}>
                 {this.state.value}
             </button>
         )
@@ -23,23 +29,69 @@ class Square extends React.Component {
 class Board extends React.Component {
     renderSquare(i) {
         return (
-            <Square value = {i}></Square>
+            <Square 
+                key={i}
+                value = {this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
         )
     }
+
     render() {
         return (
             <div>
                 <div className='board-row'>
-                    {this.renderSquare(7)}
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className='board-row'>
+                    {this.renderSquare(3)}
                     {this.renderSquare(4)}
-                    {this.renderSquare(2002)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className='board-row'>
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
                 </div>
             </div>
         )
     }
 };
 
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            players: ['X', 'O'],
+            turn: 0,
+            squares: Array(9).fill(0),
+        };
+    }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.players[this.state.turn % 2];
+        this.setState({
+            squares: squares,
+            value: this.state.players[this.state.turn % 2],
+            turn: this.state.turn + 1,
+        });
+    }
+
+    render() {
+        return (
+            <div className='status'>
+                Next Player: {this.state.players[this.state.turn % 2]}
+                <Board 
+                    squares={this.state.squares}
+                    onClick={(i) => this.handleClick(i)}
+                />
+            </div>
+        )
+    }
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Board />);
+root.render(<Game />);
